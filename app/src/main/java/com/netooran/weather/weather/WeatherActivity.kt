7f346378dd5452d6f38d2ed2747471c894cc72fc
Sarath V S <sarathvss65@gmail.com>
2018-09-21 16:26:22 +0530

    Show appropriate icon for the primary weather condition

--- app/src/main/java/com/netooran/weather/weather/WeatherActivity.kt
+++ app/src/main/java/com/netooran/weather/weather/WeatherActivity.kt
@@ -4,20 +4,18 @@ import android.Manifest.permission.ACCESS_COARSE_LOCATION
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.content.pm.PackageManager
-import android.location.Geocoder
 import android.location.Location
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat.checkSelfPermission
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import android.widget.Toast
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.netooran.weather.Injections
 import com.netooran.weather.R
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
 
 class WeatherActivity : AppCompatActivity() {
     private lateinit var viewModelFactory: WeatherViewModel.ViewModelFactory
@@ -26,7 +24,6 @@ class WeatherActivity : AppCompatActivity() {
     private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     private val MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION: Int = 1
-    private val CITY_NAME_INDEX = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,12 +41,12 @@ class WeatherActivity : AppCompatActivity() {
             ActivityCompat.requestPermissions(this, arrayOf(ACCESS_COARSE_LOCATION), MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION)
 
         fusedLocationClient.lastLocation
-                .addOnFailureListener { exception -> Toast.makeText(this, exception.message, Toast.LENGTH_SHORT).show() }
+                .addOnFailureListener { Toast.makeText(this, getString(R.string.fetch_location_error), Toast.LENGTH_SHORT).show() }
                 .addOnSuccessListener { location: Location ->
                     mModel.getCurrentWeather(location)
                             .observe(this, Observer {
                                 if (it == null) {
-                                    Log.e(this.javaClass.canonicalName, "::::: Something went wrong while fetching weather. 'Weather is null'")
+                                    Toast.makeText(this, getString(R.string.fetch_weather_error), Toast.LENGTH_SHORT).show()
                                     return@Observer
                                 }
                                 populateWeather(it)
@@ -57,17 +54,14 @@ class WeatherActivity : AppCompatActivity() {
                 }
     }
 
-    private fun getCity(location: Location): String =
-            Geocoder(this, Locale.getDefault())
-                    .getFromLocation(location.latitude, location.longitude, 1)[0]
-                    .getAddressLine(CITY_NAME_INDEX).split(",")[1]
-
     private fun populateWeather(weather: Model.Weather?) {
-        temperature.text = weather?.main?.temp.toString()
-        weather_text.text = weather?.weather!![0].description
-        weather_icon.background = getDrawable(getIcon(weather))
+        temperature.text = weather!!.main.temp.toString()
         location.text = weather.name
+        weather_text.text = weather.weather[0].description
+
+        Picasso.with(this).load("http://openweathermap.org/img/w/${weather.weather[0].icon}.png")
+                .resize(300, 0)
+                .into(weather_icon)
     }
 
-    fun getIcon(weather: Model.Weather?) = R.drawable.ic_01_s // Fetch icon from openWeatherMap
 }
